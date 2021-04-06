d86368aa9d163969c0168bc82fe8a733dae321a3
r4pit <r4pidstart@gmail.com>
2021-04-06 23:00:40 +0900

    ?

--- test.cpp
+++ test.cpp
@@ -1,25 +1,9 @@
 
 #include<iostream>
-#include<algorithm>
 #include<vector>
 using namespace std;
 
 int main(void)
 {
-    int n; scanf("%d", &n);
-    int num[n];
-    vector<int> sorted(n);
 
-    for(int i=0; i<n; i++)
-    {    
-        int tmp; scanf("%d", &tmp);
-        sorted[i]=tmp;
-        num[i]=tmp;
-    }
-
-    sort(sorted.begin(), sorted.end());
-    sorted.erase(unique(sorted.begin(), sorted.end()), sorted.end()); 
-
-    for(int i=0; i<n; i++)
-        printf("%d ", lower_bound(sorted.begin(), sorted.end(), num[i])-sorted.begin());
 }
